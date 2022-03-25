b08eec1d8f5edbf46a47ec1259c62a33ad8edb60
Tayná Porfiro <15tayna.porfiro@gmail.com>
2022-03-25 16:29:23 -0300

    Cabeçalho do projeto

---
+++ App.js
@@ -0,0 +1,20 @@
+import Cabecalho from './src/components/cabecalho';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Cabecalho></Cabecalho>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
